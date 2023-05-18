93d5ba6ddf7cfa32ac5e779897ffcd77fa176ea7
Joe Bedford <joseph.r.bedford@gmail.com>
2023-05-18 17:25:11 +0100

    basic functionality and style done

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,18 @@
+// Constants
+
 const WEATHER_API_BASE_URL = 'https://api.openweathermap.org';
 const WEATHER_API_KEY = '4b1a5523b5977bf435f0ee1ea815249c';
 const MAX_DAILY_FORECAST = 5;
-
-
-var dayObject = {};
-var currentObject = {};
-// var iconImg.src = iconUrl;
-
 const inputBox = document.getElementById('inputBox');
 const searchButton = document.getElementById('searchButton')
 const itemList = document.getElementById('itemList');
 
-searchButton.addEventListener('click', handleSearchButtonClick);
-inputBox.addEventListener('keydown', handleKeyDown);
+// Global Variables
 
+var dayObject = {};
+var currentObject = {};
 
-
+// Handle Click on Search Button, save textbox input to inputBox var
 function handleSearchButtonClick() {
     const itemText = inputBox.value.trim();
     if (itemText) {
@@ -24,6 +21,7 @@ function handleSearchButtonClick() {
     }
   }
 
+// Handle Enter in search box. Save textbox input to inputBox var
 function handleKeyDown(event) {
     if (event.key === 'Enter') {
       const itemText = inputBox.value.trim();
@@ -34,6 +32,7 @@ function handleKeyDown(event) {
     }
   }
 
+// Add itemText contents to li in sidebar
 function addItem(itemText) {
     const listItem = document.createElement('li');
     listItem.textContent = itemText;
@@ -45,13 +44,16 @@ function addItem(itemText) {
     itemList.appendChild(listItem);
   }
 
+// Handle click on sidebar item
   function handleClick(itemText) {
     console.log('Clicked:', itemText);
     
     lookupLocation(itemText);
   }
 
+// Look up location
 function lookupLocation(search) {
+    // Clear divs for new weather info
     current.innerHTML = '';
     container.innerHTML = '';
   // Lookup the location to get the Lat/Lon
@@ -60,68 +62,67 @@ function lookupLocation(search) {
         .then(response => response.json())
         .then(data => {
 
-            console.log(data);
+            // console.log(data);
 
             // Pick the First location from the results
-            //const location = data[0];
+            // const location = data[0];
             var lat = data[0].lat;
             var lon = data[0].lon;
 
-            console.log("Latitude: " + lat + ", Longitude: "+ lon);
+            // console.log("Latitude: " + lat + ", Longitude: "+ lon);
         
 
-            // Get the Weather for the cached location
+            // Get the current Weather for the cached location
 
             var apiCurrent = `${WEATHER_API_BASE_URL}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&cnt=5&units=metric`;
             fetch(apiCurrent)
                 .then(response => response.json())
                 .then(data => {
-                    console.log(data);
+                    // console.log(data);
 
-                    currentObject = data;
+                    currentObject = data;                                      // JSON data > currentObject object
                     
-                    weatherIconCode = currentObject.weather[0].icon;
+                    weatherIconCode = currentObject.weather[0].icon;            // Get weather icon code > weatherIconCode var
 
-                    console.log("weatherIcon: " + weatherIconCode);
+                    // console.log("weatherIcon: " + weatherIconCode);
 
-                    var current = document.querySelector('#current');
+                    var current = document.querySelector('#current');           // select current ID in html for current var to display info
                     // var newElement = document.createElement('div');
 
-                    var html2Content = `<div class = "card"><p>Current conditions in ${search}</p>`;
+
+                    // create html string to display current weather
+                    var html2Content = `<div class = "card"><p><b>Current conditions in ${search}</b></p>`;
                         html2Content += `<img src = "http://openweathermap.org/img/wn/${weatherIconCode}.png" width="100" height="100"></p>`;
                         html2Content += `<p>Temperature: ${currentObject.main.temp}°c</p>`;
                         html2Content += `<p>Humidity: ${currentObject.main.humidity}%</p>`;
-                        html2Content += `<p>Windspeed: ${currentObject.wind.speed} kph</p></div>?`;
+                        html2Content += `<p>Windspeed: ${currentObject.wind.speed} kph</p></div>`;
 
                         current.innerHTML += html2Content;
                     
                 });
 
-
+            // Get forecast weather
             var apiUrl = `${WEATHER_API_BASE_URL}/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&cnt=5&units=metric`;
             console.log("From API: " + apiUrl);
             fetch(apiUrl)
                 .then(response => response.json())
                 .then(data => {
 
-                     console.log(data);
+                     //console.log(data);
 
-                    // Display the Current Weather
-
-                    // Display the 5 Day Forecast
-                    
+                    // iterate over the 5 items to                     
                         for (i = 0; i < 5; i++) {
-                            var variableName = "day" + i;
-                            dayObject[i] = data.list[i];
+                            // var variableName = "day" + i;
+                            dayObject[i] = data.list[i];                        // load iterations (days) weather into dayObject (day)
                        
-                            weatherIconCode = dayObject[i].weather[0].icon;
+                            weatherIconCode = dayObject[i].weather[0].icon;     // get weather icon code for (day)
                     // console.log(dayObject);
 
                     
-                        var container = document.querySelector('#container');
-                        // var newElement = document.createElement('div');
+                        var container = document.querySelector('#container');   // container id on webpage for display of container variable
                         
-                        var htmlContent = `<div class = "card"><p>${dayObject[i].dt_txt}</p>`;
+                        // create html for 5 cards for forecast
+                        var htmlContent = `<div class = "card"><p><b>${dayObject[i].dt_txt}</b></p>`;
                         htmlContent += `<img src = "http://openweathermap.org/img/wn/${weatherIconCode}.png" width="50" height="50">`;
                         htmlContent += `<p>Temperature: ${dayObject[i].main.temp}°c</p>`;
                         htmlContent += `<p>Humidity: ${dayObject[i].main.humidity}%</p>`;
@@ -137,3 +138,8 @@ function lookupLocation(search) {
             
         }
           
+
+        // event handlers for button and searchbox
+        searchButton.addEventListener('click', handleSearchButtonClick);
+        inputBox.addEventListener('keydown', handleKeyDown);
+        
